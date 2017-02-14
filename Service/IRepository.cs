6775f08386c9934399d04ea95356e34eadc75109
GaaraC <poyu.wang@hpe.com>
2017-02-14 11:17:30 +0800

    分层

---
+++ Service/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    interface IRepository<T> where T : class
+    {
+        void add(T entity);
+        void delete(T entity);
+        void save();
+        IEnumerable<T> findAll(Func<T, bool> exp);
+    }
+}
---
+++ Service/Repository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+        public void add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> findAll(Func<T, bool> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
